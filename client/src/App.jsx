56a361b8ecad02f9ef6bf80295660196a5f7b39f
Gerard <gerardherrera75@gmail.com>
2024-12-16 22:20:47 +0800

    Create User Model

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Header from './components/Header';
 
 export default function App() {
   return <BrowserRouter>
-    <Header/ >
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/sign-in" element={<SignIn/>}/>
