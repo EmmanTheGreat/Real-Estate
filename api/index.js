78b9853ec681fde870ea91c7f17022bf196dd720
Gerard <gerardherrera75@gmail.com>
2024-12-22 17:25:48 +0800

    Create a Middleware and Fucntion to handle possible errors

--- api/index.js
+++ api/index.js
@@ -26,3 +26,13 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.satusCode|| 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
